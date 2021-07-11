95311f7a1c6031b071f9536d5dd8a376a2962eca
HamzhSuilik <9hamzh@gmail.com>
2021-07-11 22:12:44 +0300

    Initialize react native project , create Login page

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet , Button , Text, View , Link ,TextInput} from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
+
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+
+
+
+
+const Stack = createStackNavigator();
+
+const globalScreenOption = {
+  headerStyle:{backgroundColor:"#2C6BED"},
+  headerTitleStyle:{color:"white"},
+  headerTintColor:"white",
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer >
+      <Stack.Navigator screenOptions ={globalScreenOption}>
+
+        <Stack.Screen component={LoginScreen} name="Login"></Stack.Screen>
+
+    
+      </Stack.Navigator>
+
+    </NavigationContainer>
+
   );
 }
-
+{/* <StatusBar style="auto" /> */}
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: "auto",
+    maxWidth: 500
   },
 });
+
+
