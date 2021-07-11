836f9cf89c6eca0b5dc9d723aa9f88e2249f0ded
HamzhSuilik <9hamzh@gmail.com>
2021-07-11 22:25:12 +0300

    Initialize react native project , create Login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -41,7 +41,9 @@ const LoginScreen = () => {
             </View>
            
             
+
             
+
             <Button containerStyle={styles.button} title="Login" />
             <Button containerStyle={styles.button} type="outline" title="Register" />
             
