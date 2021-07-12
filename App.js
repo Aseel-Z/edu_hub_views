994db3794e6bd786a20eb8474da52afcca026f49
HamzhSuilik <9hamzh@gmail.com>
2021-07-12 10:55:17 +0300

    push main barnch

--- App.js
+++ App.js
@@ -2,12 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet , Button , Text, View , Link ,TextInput} from 'react-native';
 import tw from 'tailwind-react-native-classnames';
-
-
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 
 
@@ -25,12 +24,9 @@ export default function App() {
 
     <NavigationContainer >
       <Stack.Navigator screenOptions ={globalScreenOption}>
-
         <Stack.Screen component={LoginScreen} name="Login"></Stack.Screen>
-
-    
+        <Stack.Screen component={RegisterScreen} name="Register"></Stack.Screen>
       </Stack.Navigator>
-
     </NavigationContainer>
 
   );
@@ -38,9 +34,6 @@ export default function App() {
 {/* <StatusBar style="auto" /> */}
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: "auto",
-    maxWidth: 500
+    flex: 1,
   },
 });
-
-
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,29 +1,30 @@
 import React , {useState} from 'react'
-
 import { View, Text , StyleSheet } from 'react-native'
 import { StatusBar } from 'expo-status-bar';
-
-
-
 import { Input , Button,Image  } from 'react-native-elements';
-// import { Input } from 'react-native-elements/dist/input/Input';
-
 import tw from 'tailwind-react-native-classnames';
+import { KeyboardAvoidingView } from 'react-native';
 
 
 
-const LoginScreen = () => {
-    const [email,setEmail]=useState("")
-    const [password,setPassword]=useState("")
+const LoginScreen = ({navigation}) => {
 
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
 
+    const signIn = () => {
+
+    }
 
     return (
-        <View>
+
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
             <StatusBar style="light" />
             
-            <Image style={{width:200 , height:200 }} source={{uri:"https://image.flaticon.com/icons/png/512/3256/3256114.png"}}/>  
-
+            <Image style={{width:200 , height:200, marginBottom:30 }} source={{uri:"https://image.flaticon.com/icons/png/512/3256/3256114.png"}}/>  
+            <Text style={{marginBottom:30 , fontSize:30, fontWeight:'bold', color:"#2C6BED"}}>
+                EDU HUB
+            </Text>
             <View style={styles.inputContainer}>
                <Input
                 placeholder="Email"
@@ -40,22 +41,27 @@ const LoginScreen = () => {
                onChange={(pass)=>{setPassword(pass)}} />  
             </View>
            
-            
-
-            
-
-            <Button containerStyle={styles.button} title="Login" />
-            <Button containerStyle={styles.button} type="outline" title="Register" />
-            
-        </View>
+            <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+            <Button containerStyle={styles.button} onPress={() => navigation.navigate('Register')} type="outline" title="Register" />
+            <View style={{height:30}} />
+        </KeyboardAvoidingView>
     )
 }
 
 export default LoginScreen
 
 const styles = StyleSheet.create({
-    inputContainer:{},
-    button:{}
+    inputContainer:{
+        width:300
+    },
+    button:{
+        width:200,
+        marginTop:20
+    },
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        padding: 10
+    }
   });
-
-  
