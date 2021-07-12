75092d8099d8827e5925bcb57919f81ff671582b
HishamKhalil1990 <hisham.khalil1990@gmail.com>
2021-07-12 23:25:49 +0300

    add home screen and api's functions

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,39 +1,42 @@
 import React , {useState} from 'react'
-import { StyleSheet,View } from 'react-native'
-import { Input, Button, Text, SearchBar } from 'react-native-elements';
+import { StyleSheet,View} from 'react-native'
+import { Input, Text, Button,Image  } from 'react-native-elements';
 import { Dimensions } from 'react-native';
-
+import tw from 'tailwind-react-native-classnames';
 
 const HomeScreen = ({navigation,route}) => {
     
     const devHight = Dimensions.get('window').height;
     const devWidth = Dimensions.get('window').width;
-    const [search,setSearch] = useState('')
+    const viewWidth = devWidth > 600? 700 : devWidth
 
     return (
         <View style={styles.container}>
-            <View style={{ width:devWidth > 600? 600 : devWidth}}>
-                <SearchBar 
-                    containerStyle={{ marginTop:10}}
-                    inputContainerStyle= {{ }}
-                    placeholder="Type Here..."
-                    value={search}
-                    onChangeText={(text) => setSearch(text)}
-                />
+            <View style={{ width:viewWidth,height:devHight}}>
+                <View style={tw`mt-4 flex flex-col items-center`}>
+                    <View style={tw`flex cursor-pointer flex flex-row`}>
+                        <Button style={tw`mr-1`} type={'outline'}title={'Home'}/>
+                        <Button style={tw`mr-1`} title={'Profile'}/>
+                        <Button style={tw`mr-1`} title={'Search'}/>
+                        <Button style={tw`mr-1`} title={'Sign Out'}/>
+                    </View>
+                </View>
+                <View style={tw`w-full h-full flex flex-col items-center mt-10`}>
+                    <View style={tw`h-1/4 w-11/12 border rounded-md flex flex-col justify-center`}>
+                        <Image style={{width:100 , height:100}} source={{uri:"https://image.flaticon.com/icons/png/512/3135/3135715.png"}}/>
+                    </View>
+                </View>
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center',
-        // justifyContent:'center',
-    }
+    container:tw`flex items-center`
 })
 
 export default HomeScreen
 
 
 // {console.log(route.params.username),console.log(route.params.pass)}
+
