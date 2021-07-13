46b12d6374d4eebcfd08c73e88d3f8794557b223
Aseel-Z <aseelalzweri@gmail.com>
2021-07-13 13:47:20 +0300

    test socket

--- App.js
+++ App.js
@@ -11,6 +11,10 @@ import HomeScreen from './screens/HomeScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import SearchScreen from './screens/SearchScreen';
 import ResultScreen from './screens/ResultScreen';
+import MessagesScreen from './screens/MessagesScreen';
+
+window.navigator.userAgent = 'react-native';
+import io from 'socket.io-client/dist/socket.io';
 
 
 const Stack = createStackNavigator();
@@ -19,23 +23,28 @@ const globalScreenOption = {
   headerStyle:{backgroundColor:"#2C6BED"},
   headerTitleStyle:{color:"white"},
   headerTintColor:"white",
-}
+};
 
 export default function App() {
 
-  const [username,setUsername] = useState('')
-  const [pass,setPass] = useState('')
+  constructor(){
+    super();
+    this.socket = io('localhost:3000', {jsonp:false});}
+
+  const [username,setUsername] = useState('');
+  const [pass,setPass] = useState('');
   
   return (
 
     <NavigationContainer >
       <Stack.Navigator screenOptions ={globalScreenOption}>
+        <Stack.Screen component={LoginScreen} initialParams={{setUsername:setUsername,setPass:setPass}} name="Login"></Stack.Screen>
+        <Stack.Screen component={RegisterScreen} initialParams={{setUsername:setUsername,setPass:setPass}} name="Register"></Stack.Screen>
         <Stack.Screen component={ResultScreen} data={{}} name="Result"></Stack.Screen>
         <Stack.Screen component={ProfileScreen} data={{}} name="Profile"></Stack.Screen>
         <Stack.Screen component={SearchScreen} data={{}} name="Search"></Stack.Screen>
-        <Stack.Screen component={LoginScreen} initialParams={{setUsername:setUsername,setPass:setPass}} name="Login"></Stack.Screen>
-        <Stack.Screen component={RegisterScreen} initialParams={{setUsername:setUsername,setPass:setPass}} name="Register"></Stack.Screen>
         <Stack.Screen component={HomeScreen} initialParams={{username:username,pass:pass}} name="Home"></Stack.Screen>
+        <Stack.Screen component={MessagesScreen} data={{}} name="Messages"></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
 
