070fae85b3a65ba16189c296c28445855fd3788a
Aseel-Z <aseelalzweri@gmail.com>
2021-07-13 13:33:52 +0300

    socket.io

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+ '/index.html')
+    
+});
+
+io.on('connection', function(socket) {
+    
+})
+// const sessionMiddleware = session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }});
+// // register middleware in Express
+// app.use(sessionMiddleware);
+// // register middleware in Socket.IO
+// io.use((socket, next) => {
+//   sessionMiddleware(socket.request, {}, next);
+//   // sessionMiddleware(socket.request, socket.request.res, next); will not work with websocket-only
+//   // connections, as 'socket.request.res' will be undefined in that case
+// });
+
+// io.on('connection', (socket) => {
+//   const session = socket.request.session;
+//   session.connections++;
+//   session.save();
+// });
+
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => console.log('server listening on port ' + port));
